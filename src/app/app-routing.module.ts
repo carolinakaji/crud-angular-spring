656d9cfc8c7d6e5888f91511aad5f4f4f5db1a73
Carolina Kaji <caii@gft.com>
2021-11-25 16:05:57 -0300

    Acompanhamento curso - videos até 05

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /**path: indica o caminho
+    *pathMatch: diz ao router como juntar a URL
+  */
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  /**Utilizando lazy loading
+    * loadChildren: carregar o módulo filho de forma automática
+    * .then: indica qual o módulo para carregar
+  */
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
